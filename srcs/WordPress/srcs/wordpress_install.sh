7b2a1311d30078420543ac8109799d79c521a5d7
Abderrahmane Sfaihi <asfaihi@e2r4p1.1337.ma>
2021-03-26 14:52:53 +0100

    Changed setup of nginx.conf files

--- srcs/WordPress/srcs/wordpress_install.sh
+++ srcs/WordPress/srcs/wordpress_install.sh
@@ -4,7 +4,7 @@ cd /var/www/localhost/ft_services/
 
 wget https://wordpress.org/latest.tar.gz
 
-tar xvf latest.tar.gz
+tar -xvf latest.tar.gz
 
 rm -rf latest.tar.gz /var/www/localhost/ft_services/wordpress/wp-config-sample.php
 
