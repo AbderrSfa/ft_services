1d09f8a1dae12d1e915ff537a37bebcbff6b961b
Abderrahmane Sfaihi <asfaihi@e2r4p6.1337.ma>
2021-03-27 13:27:37 +0100

    Added InfluxDB

---
+++ srcs/InfluxDB/srcs/InfluxDB_install.sh
@@ -0,0 +1,9 @@
+cd var/www/localhost/ft_services/
+
+wget https://dl.influxdata.com/influxdb/releases/influxdb2-2.0.4-linux-amd64.tar.gz
+
+tar -xvf influxdb2-2.0.4-linux-amd64.tar.gz 
+
+rm -rf influxdb2-2.0.4-linux-amd64.tar.gz 
+
+mv influxdb2-2.0.4-linux-amd64/ influxdb
