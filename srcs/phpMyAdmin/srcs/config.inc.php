e70e57e42f6466cc4bf3883133b1cedef16815e3
Abderrahmane Sfaihi <abderrahmanesfaihi@hotmail.com>
2021-04-16 17:00:00 +0000

    Working on proxy, and minikube local images

--- srcs/phpMyAdmin/srcs/config.inc.php
+++ srcs/phpMyAdmin/srcs/config.inc.php
@@ -28,7 +28,7 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = 'localhost';
+$cfg['Servers'][$i]['host'] = '172.17.0.2';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
 
