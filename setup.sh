c14ffe112a10db87021f51505f298e7d04ab6608
Abderrahmane Sfaihi <abderrahmanesfaihi@hotmail.com>
2021-05-16 15:28:08 +0100

    Fixed nginx deployment, working on other deployments

--- setup.sh
+++ setup.sh
@@ -1,11 +1,9 @@
-docker-machine create --driver virtualbox default
+eval $(minikube -p minikube docker-env)
 
-eval $(docker-machine env default)
-
-docker build -t nginx ./srcs/nginx
-docker build -t grafana ./srcs/grafana
-docker build -t influxdb ./srcs/influxdb
-docker build -t mysql ./srcs/mysql
-docker build -t phpmyadmin ./srcs/phpmyadmin
-docker build -t wordpress ./srcs/wordpress
-docker build -t ftps ./srcs/ftps
+docker build -t nginx-abderr ./srcs/nginx
+docker build -t grafana-abderr ./srcs/grafana
+docker build -t influxdb-abderr ./srcs/influxdb
+docker build -t mysql-abderr ./srcs/mysql
+docker build -t phpmyadmin-abderr ./srcs/phpmyadmin
+docker build -t wordpress-abderr ./srcs/wordpress
+docker build -t ftps-abderr ./srcs/ftps
