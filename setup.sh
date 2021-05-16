7160867ad8811d345d68e5463358da00139565bc
Abderrahmane Sfaihi <abderrahmanesfaihi@hotmail.com>
2021-05-16 15:55:21 +0100

    Changed location of yaml files

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 eval $(minikube -p minikube docker-env)
 
-docker build -t nginx-abderr ./srcs/nginx
-docker build -t grafana-abderr ./srcs/grafana
-docker build -t influxdb-abderr ./srcs/influxdb
-docker build -t mysql-abderr ./srcs/mysql
-docker build -t phpmyadmin-abderr ./srcs/phpmyadmin
-docker build -t wordpress-abderr ./srcs/wordpress
-docker build -t ftps-abderr ./srcs/ftps
+docker build -t nginx:abderr ./srcs/nginx
+docker build -t grafana:abderr ./srcs/grafana
+docker build -t influxdb:abderr ./srcs/influxdb
+docker build -t mysql:abderr ./srcs/mysql
+docker build -t phpmyadmin:abderr ./srcs/phpmyadmin
+docker build -t wordpress:abderr ./srcs/wordpress
+docker build -t ftps:abderr ./srcs/ftps
