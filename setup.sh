c9ede24991b71898a761d543c0fd253830d694f8
Abderrahmane Sfaihi <abderrahmanesfaihi@hotmail.com>
2021-05-26 16:53:56 +0100

    Added ftps passive port

--- setup.sh
+++ setup.sh
@@ -20,8 +20,8 @@ docker build -t ftps:abderr ./srcs/ftps
 kubectl apply -f ./srcs/metallb.yaml
 kubectl apply -f ./srcs/mysql.yaml
 kubectl apply -f ./srcs/influxdb.yaml
-kubectl apply -f ./srcs/nginx.yaml
-kubectl apply -f ./srcs/wordpress.yaml
 kubectl apply -f ./srcs/phpmyadmin.yaml
+kubectl apply -f ./srcs/wordpress.yaml
 kubectl apply -f ./srcs/grafana.yaml
+kubectl apply -f ./srcs/nginx.yaml
 kubectl apply -f ./srcs/ftps.yaml
